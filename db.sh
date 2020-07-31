988296a0dffd06e6ceb29c0c6a85cc71ec747204
Karol Kreński <mimoohowy@gmail.com>
2020-07-31 16:35:25 +0000

    minor

--- db.sh
+++ db.sh
@@ -1,5 +1,5 @@
-echo "show tables"                                             | mysql sgsp
-echo "select * from wp_users where user_email REGEXP 'mim.*m'" | mysql sgsp
-echo "describe wp_users"                                       | mysql sgsp
-echo "update wp_users set user_pass=md5('alamakotaipsa')"      | mysql sgsp
-echo "update wp_users set user_login='mimooh'"                 | mysql sgsp
+echo "show tables"                                             | mm sgsp
+echo "select * from wp_users where user_email REGEXP 'mim.*m'" | mm sgsp
+echo "select * from sgsp_posts limit 5"                                  | mm sgsp
+echo "describe wp_users"                                       | mm sgsp
+echo "update wp_users set user_pass=md5('alamakotaipsa')"      | mm sgsp
